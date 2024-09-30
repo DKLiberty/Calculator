8a21a10148ed28fff937835df570edc5a8ec2b38
Armen Reinhardt <armenrei@ti-t2.skd.21-school.uz>
2024-09-30 15:19:26 +0500

    Base

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import requests
+
+money = int(input("""
+USD to ???
+1. UZS
+2. RUB
+3. TRY
+\nSelect: """))
+
+match money:
+    case 1:
+        money = "UZS"
+    case 2:
+        money = "RUB"
+    case 3:
+        money = "TRY"
+
+
+value = int(input("\nValue: "))
+
+def get_exchange_rate(api_key, base_currency="USD"):
+    url = f"https://api.exchangerate-api.com/v4/latest/{base_currency}"
+    response = requests.get(url)
+    data = response.json()
+    return data["rates"]
+
+def convert_currency(amount, rate_from, rate_to):
+    return amount * (rate_to / rate_from)
+
+rates = get_exchange_rate(api_key="b1413fd51e124e848f9e5ccfe509d79d")
+user_money = convert_currency(value, rates["USD"], rates[money])
+
+formatted_amount = f"{user_money:,}".replace(",", " ")
+print(f"{value} USD = {formatted_amount} {money}")
